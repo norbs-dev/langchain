524a81bee47b4ba9f0f7226ba6a170a9062332e8
norbert <norbert.cs9@gmail.com>
2025-10-22 18:07:22 -0300

    hello world chain

--- eden_marko_udemy/main.py
+++ eden_marko_udemy/main.py
@@ -1,13 +1,24 @@
 import os
-
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 
 def main():
-    print("Hello from eden-marko-udemy!")
-    print(os.environ.get("OPENAI_API_KEY"))
+    llm = ChatOllama(temperature=0, model='gemma3:270m')
+    information = input('What you need to know: ')
+    summary_template = "Give the information {information} create a summarize answer about it and fix the English grammar if is there any error"
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+    chain = summary_prompt_template | llm
+    response = chain.invoke(input={"information": information})
+    print(response.content)
+
+    
+    
 
 
 if __name__ == "__main__":
